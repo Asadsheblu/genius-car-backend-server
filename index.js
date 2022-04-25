45d48dd433dbef03dd0716c614e225c233b0755d
Asadsheblu <shebluasad@gmail.com>
2022-04-26 00:15:24 +0600

    some-service-add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors=require("cors")
 const app = express()
+var jwt = require('jsonwebtoken');
+
 require('dotenv').config()
 const port =process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -8,6 +10,22 @@ const { query } = require('express')
 //midleware
 app.use(cors())
 app.use(express.json())
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:"unauthorized access"})
+    }
+    const token=authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+        if(err){
+            return res.status(403).send({message:"forbidden access"})
+        }
+        console.log(decoded);
+        req.decoded=decoded
+    })
+    console.log(authHeader);
+    next()
+}
 app.get('/',(req,res)=>{
     res.send('hellow server')
 })
@@ -24,7 +42,19 @@ async function run() {
         await client.connect();
         const collection=client.db("Genius-car").collection("service")
         const bikeCollection=client.db("Genius-car").collection("bike")
-        const reviewCollection=client.db("Genius-car").collection("review")
+        const reviewCollection=client.db("Genius-car").collection
+        ("review")
+        const orderCollection=client.db("Genius-car").collection
+        ("order")
+        //auth
+        app.post('/token',async(req,res)=>{
+           
+            const user=req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN,{
+                expiresIn:"1d"
+            });
+            res.send({token})
+        })
         //multiple data for service  
         app.get('/service',async (req,res)=>{
             const query = {}
@@ -72,6 +102,26 @@ async function run() {
             const filter={_id:ObjectId(id)}
             const deleteresult=await collection.deleteOne(filter)
             res.send(deleteresult)
+        })
+        //create order
+        app.post('/order',async(req,res)=>{
+            const query=req.body
+            const result=await orderCollection.insertOne(query)
+            res.send(result)
+        })
+        //get order api
+        app.get('/order',verifyJWT,async(req,res)=>{
+            const decodedEmail=req.decoded.email
+            const email=req.query.email
+           if(email===decodedEmail){
+            const query={email:email}
+            const orderResult=orderCollection.find(query)
+            const result=await orderResult.toArray()
+            res.send(result)
+           }
+           else{
+            return res.status(403).send({message:"forbidden access"})
+           }
         })
     }
     finally{
